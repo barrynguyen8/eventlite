8675973ae038257db31f58ba4bcd1e1c6ebcafe9
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-07-02 00:35:17 +1000

    Event data model added

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,26 +1,5 @@
 class EventsController < ApplicationController
     def index
-        @events = [
-          {
-            title: "7 Myths About Preventing Lower Back Pain",
-            datetime: "Thursday 1 Jul, 2021",
-            location: "Acland Street Physiotherapy"
-          },
-          {
-            title: "Reversing Diabetes Type II through Diet",
-            datetime: "Tuesday 15 Aug, 2021",
-            location: "Live Webinar"
-          },
-          {
-            title: "Evidence Based Techniques on Improving Sleep Quality",
-            datetime: "Friday 25 Oct, 2021",
-            location: "Pre-recorded Webinar"
-          },
-          {
-            title: "7 Less Known Benefits of practicing Mindfulness",
-            datetime: "Saturday 26 Dec, 2021",
-            location: "Austin Hospital"
-          }
-        ]
+        @events = Event.order('start_datetime ASC')
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+events = Event.create([
+    {
+        title: "7 Myths About Preventing Lower Back Pain",
+        start_datetime: "Thu, 1 Jul 2021",
+        location: "Acland Street Physiotherapy"
+    },
+    {
+        title: "Reversing Diabetes Type II through Diet",
+        start_datetime: "Tue, 15 Aug 2021",
+        location: "Live Webinar"
+    },
+    {
+        title: "Evidence Based Techniques on Improving Sleep Quality",
+        start_datetime: "Fri, 25 Oct 2021",
+        location: "Pre-recorded Webinar"
+    },
+    {
+        title: "7 Less Known Benefits of practicing Mindfulness",
+        start_datetime: "Sat, 26 Dec 2021",
+        location: "Austin Hospital"
+    }
+  ])
