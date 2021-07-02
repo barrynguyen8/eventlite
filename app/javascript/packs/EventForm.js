03ee684b8cdba58da647d820b86a1fe402697d58
Barry Nguyen <barry.tuan.nguyen@gmail.com>
2021-07-02 11:14:38 +1000

    handling form submissions updated

--- app/javascript/packs/EventForm.js
+++ app/javascript/packs/EventForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 class EventForm extends React.Component {
     constructor(props) {
@@ -10,19 +11,36 @@ class EventForm extends React.Component {
         }
       }
 
-      handleInput = (event) => {
-        const name = event.target.name
-        const newState = {}
-        newState[name] = event.target.value
-        this.setState(newState)
-        event.preventDefault()
-      }
-    
+    handleInput = (event) => {
+    const name = event.target.name
+    const newState = {}
+    newState[name] = event.target.value
+    this.setState(newState)
+    event.preventDefault()
+    }
+
+    handleSubmit = e => {
+    axios({
+        method: 'POST',
+        url: '/events',
+        data: { event: this.state },
+        headers: {
+        'X-CSRF-Token': document.querySelector("meta[name=csrf-token]").content
+        }
+    })
+    .then(response => {
+        this.props.handleNewEvent(response.data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+    e.preventDefault()
+    }
   render () {
     return (
       <div>
         <h4>Create an Event:</h4>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input type="text" name="title" placeholder="Title" value={this.state.title} onChange={this.handleInput} />
           <input type="text" name="start_datetime" placeholder="Date" value={this.state.start_datetime} onChange={this.handleInput} />
           <input type="text" name="location" placeholder="Location" value={this.state.location} onChange={this.handleInput} />
